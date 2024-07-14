82866adf8a860d5da4c7c229c8b6e04d866b1c52
Khaled Nadam <khalednadam@gmail.com>
2024-07-14 17:14:31 +0300

    feat: error handling for expense creation

--- server/api/expense/index.post.ts
+++ server/api/expense/index.post.ts
@@ -1,16 +1,38 @@
 import { getServerSession, getToken } from "#auth";
+import httpStatus from "http-status";
 import { Expense } from "../models/expense.model";
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event);
   if (!session) {
-    return { status: "unauthenticated!" };
+    throw createError({
+      message: "You are unauthenticated!",
+      statusCode: httpStatus.UNAUTHORIZED,
+    });
   }
   const body = await readBody(event);
+  if (!body.amount) {
+    throw createError({
+      message: "Please enter the amount",
+      statusCode: httpStatus.BAD_REQUEST,
+    });
+  }
+  if (!body.category) {
+    throw createError({
+      message: "Please enter the category",
+      statusCode: httpStatus.BAD_REQUEST,
+    });
+  }
+  if (!body.type) {
+    throw createError({
+      message: "Please enter the type",
+      statusCode: httpStatus.BAD_REQUEST,
+    });
+  }
   const expense = await Expense.create({ ...body, user: session.user._id });
   if (session.user !== undefined) {
     console.log("session: ", session.user._id);
   }
   return {
-    uid: session.user._id,
+    expense,
   };
 });
