577bca682de8bf620b83a7b8c7df58d6617ed977
Khaled Nadam <khalednadam@gmail.com>
2024-07-14 14:35:27 +0300

    fix: remove unused imports and dec

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,13 +3,9 @@ definePageMeta({
   layout: "app",
   middleware: "auth",
 });
-
-const { data } = useAuth();
 </script>
 <template>
   <NuxtLayout>
-    <h1 class="text-3xl font-bold">
-      Dashboard
-    </h1>
+    <h1 class="text-3xl font-bold">Dashboard</h1>
   </NuxtLayout>
 </template>
