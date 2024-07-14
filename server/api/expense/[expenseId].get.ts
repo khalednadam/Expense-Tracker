d1a8c249849ba472355edbb1afd5ed8b7ed5bd33
Khaled Nadam <khalednadam@gmail.com>
2024-07-14 17:25:05 +0300

    feat: get expense by id

---
+++ server/api/expense/[expenseId].get.ts
@@ -0,0 +1,26 @@
+import { getServerSession } from "#auth";
+import httpStatus from "http-status";
+import { Expense } from "../models/expense.model";
+export default defineEventHandler(async (event) => {
+  const session = await getServerSession(event);
+  if (!session) {
+    throw createError({
+      message: "You are unauthenticated!",
+      statusCode: httpStatus.UNAUTHORIZED,
+    });
+  }
+
+  const expenseId = getRouterParam(event, "expenseId");
+  const expense = await Expense.findById(expenseId);
+
+  if (expense && expense.user !== session.user._id) {
+    throw createError({
+      message: "You are not authorized",
+      statusCode: httpStatus.UNAUTHORIZED,
+    });
+  }
+
+  return {
+    expense,
+  };
+});
