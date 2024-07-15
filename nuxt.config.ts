4261367c700d760f281a0d32d5c44bcceb9d496b
Khaled Nadam <khalednadam@gmail.com>
2024-07-15 20:06:50 +0300

    config: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "nuxt-mongoose",
+    "@pinia/nuxt",
     "@sidebase/nuxt-auth",
     "@nuxtjs/tailwindcss",
     "vuetify-nuxt-module",
