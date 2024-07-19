15d09ccf03df226681760c6366672f2fdbaa186d
Khaled Nadam <khalednadam@gmail.com>
2024-07-19 23:46:14 +0300

    fix: header

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -5,24 +5,19 @@ const { data, signOut } = useAuth();
   <v-app-bar elevation="0">
     <v-container>
       <v-row class="flex items-center">
-        <v-col cols="2">
-          <p class="font-bold text-xl">Expense Tracker</p>
+        <v-col cols="6">
+          <p class="font-bold text-xl">Khaled's Expense Tracker</p>
         </v-col>
-        <v-col cols="8" class="mx-auto flex flex-col">
+        <!-- <v-col cols="8" class="mx-auto flex flex-col">
           <v-list nav class="flex items-center space-x-4 mx-auto">
             <NuxtLink to="/dashboard">
               <v-list-item :active="$route.name === 'dashboard'" @click="">
                 Dashboard
               </v-list-item>
             </NuxtLink>
-            <NuxtLink to="/settings">
-              <v-list-item :active="$route.name === 'settings'" @click="">
-                Settings
-              </v-list-item>
-            </NuxtLink>
           </v-list>
-        </v-col>
-        <v-col cols="2">
+        </v-col> -->
+        <v-col cols="6" class="justify-end flex">
           <v-list>
             <v-list-item class="w-max" @click="signOut" base-color="error">
               Sign out
