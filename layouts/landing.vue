89cc7ed901f4fc1c0806e9bf9dd2751deb90acdf
Khaled Nadam <khalednadam@gmail.com>
2024-07-19 23:46:23 +0300

    feat: landing page

---
+++ layouts/landing.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <v-container class="h-full flex justify-center items-center">
+      <slot />
+    </v-container>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: "auth",
   middleware: "auth",
+  layout: "landing",
   auth: {
     unauthenticatedOnly: true,
     navigateAuthenticatedTo: "/dashboard",
@@ -10,20 +10,35 @@ definePageMeta({
 </script>
 <template>
   <NuxtLayout>
-    <v-card class="w-1/2">
-      <v-card-title> Welcome</v-card-title>
-      <v-card-text
-        class="flex flex-col w-full justify-center items-center mx-auto p-10"
+    <v-row class="h-full">
+      <v-col
+        cols="12"
+        md="6"
+        class="h-full justify-center items-start flex flex-col"
       >
-        <div class="w-full flex flex-col space-y-4 justify-center items-center">
-          <NuxtLink to="/register" class="w-full">
-            <v-btn class="w-full"> Register </v-btn>
-          </NuxtLink>
+        <h1 class="text-7xl font-bold italic text-primary">
+          Welcome to this <span class="font-thin"> simple </span>
+          expense tracker
+        </h1>
+        <div class="flex mt-20 gap-6">
           <NuxtLink to="/login" class="w-full">
-            <v-btn class="w-full">Login</v-btn>
+            <v-btn size="x-large" variant="flat" color="primary"> Login </v-btn>
+          </NuxtLink>
+          <NuxtLink to="/register" class="w-full">
+            <v-btn size="x-large" variant="outlined" color="primary">
+              Register
+            </v-btn>
           </NuxtLink>
         </div>
-      </v-card-text>
-    </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card class="h-full relative" color="primary">
+          <v-card-title> </v-card-title>
+          <v-card-text class="absolute bottom-0 w-full text-center"
+            >All rights reserver - Khaled Nadam</v-card-text
+          >
+        </v-card>
+      </v-col>
+    </v-row>
   </NuxtLayout>
 </template>
