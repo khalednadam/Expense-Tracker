8d21dd36cfc573ff87cebbf2dcfecc37ea875291
Khaled Nadam <khalednadam@gmail.com>
2024-07-23 11:53:43 +0300

    config: add base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  app: {
+    baseURL: process.env.AUTH_ORIGIN,
+  },
   runtimeConfig: {
     authSecret: process.env.AUTH_SECRET,
   },
