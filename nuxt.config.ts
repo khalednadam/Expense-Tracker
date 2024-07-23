49f66c7be7c1f20ba2574a2c3853d187efc58baf
Khaled Nadam <khalednadam@gmail.com>
2024-07-23 16:39:42 +0300

    fix: remove baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  app: {
-    baseURL: "https://expense-tracker-indol-omega.vercel.app/",
-  },
   runtimeConfig: {
     authSecret: process.env.AUTH_SECRET,
   },
